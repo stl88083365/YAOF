6a638d1c54cd6ecdff2fa52db397ea968067dcd3
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2024-12-10 23:53:26 +0800

    enable self test

--- SCRIPTS/R2S/02_target_only.sh
+++ SCRIPTS/R2S/02_target_only.sh
@@ -14,12 +14,12 @@ sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/
 
 # let rk3328 boot at powersave mode and use luci-app-cpufreq to set back schedutil
 # this may fix 1.6g boot failure
-sed -i '/CONFIG_CPU_FREQ_DEFAULT_GOV/d' target/linux/rockchip/armv8/config-6.6
+sed -i '/CONFIG_CPU_FREQ_DEFAULT_GOV/d' target/linux/rockchip/armv8/config-5.15
 echo '
 # CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE is not set
 # CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL is not set
 CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE=y
-' >>./target/linux/rockchip/armv8/config-6.6
+' >>./target/linux/rockchip/armv8/config-5.15
 
 #Vermagic
 latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][3-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
